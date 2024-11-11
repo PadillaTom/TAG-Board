eb544532679df85a87116bd1a862df9a05d384fe
Tomas Padilla <padillatomasagustin@gmail.com>
2024-11-11 13:41:21 +0100

    develop - Testing DVH for inner height

--- app/(landing)/layout.tsx
+++ app/(landing)/layout.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const LandingLayout = ({children}: { children: React.ReactNode; }) => {
     return (
-        <div className="h-screen bg-slate-100">
+        <div className="h-dvh bg-slate-100">
             {/* Navbar */}
             <main className="h-full bg-slate-100">
             {children}
