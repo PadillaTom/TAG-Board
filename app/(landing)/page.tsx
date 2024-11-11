b8f67f499561f9c3de6f30856b6cc8c778b5da42
Tomas Padilla <padillatomasagustin@gmail.com>
2024-11-11 15:28:00 +0100

    develop - Testing styles

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -1,20 +1,27 @@
-import bullseye from "../../public/images/bullseye.png"
-import Image from "next/image";
+import Image from "next/image"
+import {Poppins} from "next/font/google";
+import {cn} from "@/lib/utils";
+
+import bullseye from "../../public/images/bullseye.png";
+
+const textFont = Poppins({
+    subsets: ["latin"],
+    weight: ["100", "200", "600"]
+})
 
 const LandingPage = () => {
     return (
         <div className="h-full flex items-center justify-around flex-col">
             <div className="flex items-center justify-around flex-col">
                 <div
-                    className="
-                    flex items-center border-0
-                    shadow-lg
-                    rounded-full
-                    py-3 px-5
-                    bg-white
-                    text-amber-700 text-lg tracking-wide font-extralight
-                    ">
-                    <Image src={bullseye} alt="TAG Board" className="mb-0.5 mr-4 max-w-8 object-cover"></Image>
+                    className={cn(
+                        "flex items-center " +
+                        "border-0 shadow-lg rounded-full py-3 px-5 " +
+                        "bg-white " +
+                        "text-amber-700 text-lg tracking-wide font-extralight",
+                        textFont.className
+                    )}>
+                    <Image src={bullseye} alt="TAG Board" className="mb-0.5 mr-4 max-w-8 object-cover"/>
                     TAG Board
                 </div>
             </div>
