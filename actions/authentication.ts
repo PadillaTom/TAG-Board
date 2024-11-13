7ba8adbe61fd08e1df650252ece1065ca9468401
Tomas Padilla <padillatomasagustin@gmail.com>
2024-11-13 11:50:24 +0100

    [TBF-2] - Handling authentication error

--- actions/authentication.ts
+++ actions/authentication.ts
@@ -1,24 +1,27 @@
 "use server";
 
-import {cookies} from "next/headers";
+import { cookies } from "next/headers";
 
 const BASE_URL = "http://localhost:8080";
 const LOGIN_URL = "/api/v1/auth/login";
 
-export const login = async (values: any) => {
-    const response = await fetch(BASE_URL + LOGIN_URL, {
-        method: "POST",
-        headers: {"Content-Type": "application/json"},
-        body: JSON.stringify({username: values.username, password: values.password}),
-    })
-    const cookieStore = await cookies()
-    const data = await response.json();
+export const login = async (values: { username: string; password: string }) => {
+	const response = await fetch(BASE_URL + LOGIN_URL, {
+		method: "POST",
+		headers: { "Content-Type": "application/json" },
+		body: JSON.stringify({ username: values.username, password: values.password }),
+	});
+	const cookieStore = await cookies();
+	const data = await response.json();
 
-    if (response.status === 202) {
-        const token = data.jwt;
-        // Store JWT in an HttpOnly cookie
-        cookieStore.set("jwt", token, {httpOnly: true});// Access the JWT from the response body
-    }
+	if (response.status === 401) {
+		return { error: "Usuario o contraseña incorrectos." };
+	}
 
-    console.log(data);
-}
+	if (response.status === 202) {
+		const token = data.jwt;
+		cookieStore.set("jwt", token, { httpOnly: true });
+	}
+
+	console.log(data);
+};
--- app/(platform)/(authentication)/login/page.tsx
+++ app/(platform)/(authentication)/login/page.tsx
@@ -1,83 +1,90 @@
 "use client";
 
 import * as z from "zod";
-import {useForm} from "react-hook-form";
-import React, {useTransition} from "react";
-import {Form, FormControl, FormField, FormItem, FormLabel, FormMessage} from "@/components/ui/form";
-import {zodResolver} from "@hookform/resolvers/zod";
-import {Input} from "@/components/ui/input";
-import {Button} from "@/components/ui/button";
+import { useForm } from "react-hook-form";
+import React, { useTransition } from "react";
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
 import Link from "next/link";
 import { login } from "@/actions/authentication";
 
-const formSchema = z.object({
-    username: z.string()
-        .min(6, {message: "Mínimo 6 caracteres"})
-        .email({message: "El email no es válido."}),
-    password: z.string()
-        .min(6, {message: "Mínimo 6 caracteres",})
-})
+const loginSchema = z.object({
+	username: z.string().min(6, { message: "Mínimo 6 caracteres" }).email({ message: "El email no es válido." }),
+	password: z.string().min(6, { message: "Mínimo 6 caracteres" }),
+});
 
 const LoginPage = () => {
-    const [isPending, startTransition] = useTransition();
+	const [isPending, startTransition] = useTransition();
 
-    const form = useForm<z.infer<typeof formSchema>>({
-        resolver: zodResolver(formSchema),
-        defaultValues: {
-            username: "",
-            password: ""
-        }
-    });
+	const form = useForm<z.infer<typeof loginSchema>>({
+		resolver: zodResolver(loginSchema),
+		defaultValues: {
+			username: "",
+			password: "",
+		},
+	});
 
-    const onSubmit = (values: z.infer<typeof formSchema>) => {
-        startTransition(() => {
-            login(values);
-        })
-    }
+	const onSubmit = (values: z.infer<typeof loginSchema>) => {
+		startTransition(async () => {
+			const result = await login(values);
+			if (result && result.error) {
+				form.setError("root", {
+					type: "manual",
+					message: result.error,
+				});
+			} else {
+				// Handle successful login, like redirecting the user
+				console.log("Authentication OK!!!");
+			}
+		});
+	};
 
-    return (
-        <div className="max-w-md w-full text-center">
-            <Form {...form} >
-                <form onSubmit={form.handleSubmit(onSubmit)}>
-                    <FormField
-                        name="username"
-                        control={form.control}
-                        render={({field}) => (
-                            <FormItem>
-                                <FormLabel>Email</FormLabel>
-                                <FormControl>
-                                    <Input {...field} placeholder={"Email"} type="email" disabled={isPending}></Input>
-                                </FormControl>
-                                <FormMessage/>
-                            </FormItem>
-                        )}
-                    />
-                    <FormField
-                        name="password"
-                        control={form.control}
-                        render={({field}) => (
-                            <FormItem className="mt-5">
-                                <FormLabel>Contraseña</FormLabel>
-                                <FormControl>
-                                    <Input {...field} placeholder={"Escriba su contraseña"} type="password"
-                                           disabled={isPending}></Input>
-                                </FormControl>
-                                <FormMessage/>
-                            </FormItem>
-                        )}
-                    />
-                    <Button type="submit" variant={"default"} className="mt-10 w-auto px-12" disabled={isPending}>
-                        Login
-                    </Button>
-                </form>
-            </Form>
-            <p className="mt-7 text-sm font-thin tracking-wide text-gray-700 underline">
-                <Link href="/">
-                    Volver al inicio
-                </Link>
-            </p>
-        </div>
-    )
-}
+	return (
+		<div className="max-w-md w-full text-center">
+			<Form {...form}>
+				<form onSubmit={form.handleSubmit(onSubmit)}>
+					<FormField
+						name="username"
+						control={form.control}
+						render={({ field }) => (
+							<FormItem>
+								<FormLabel>Email</FormLabel>
+								<FormControl>
+									<Input {...field} placeholder={"Email"} type="email" disabled={isPending}></Input>
+								</FormControl>
+								<FormMessage />
+							</FormItem>
+						)}
+					/>
+					<FormField
+						name="password"
+						control={form.control}
+						render={({ field }) => (
+							<FormItem className="mt-5">
+								<FormLabel>Contraseña</FormLabel>
+								<FormControl>
+									<Input {...field} placeholder={"Escriba su contraseña"} type="password" disabled={isPending}></Input>
+								</FormControl>
+								<FormMessage />
+							</FormItem>
+						)}
+					/>
+
+					<Button type="submit" variant={"default"} className="mt-10 w-auto px-12" disabled={isPending}>
+						Login
+					</Button>
+					{form.formState.errors.root && (
+						<FormMessage className="mt-2">{form.formState.errors.root.message}</FormMessage>
+					)}
+				</form>
+			</Form>
+			<p className="mt-7 text-sm font-thin tracking-wide text-gray-700 underline">
+				<Link href="/">Volver al inicio</Link>
+			</p>
+		</div>
+	);
+};
 
 export default LoginPage;
