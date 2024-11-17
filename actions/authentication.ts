a5aff5294f0da4f354d0adf0b6cb4eca2eec6f69
Tomas Padilla <padillatomasagustin@gmail.com>
2024-11-17 15:06:07 +0100

    deploy - Fixed REsponse from fetch to NETLIFY, in local development will appear a NextJS error

--- actions/authentication.ts
+++ actions/authentication.ts
@@ -4,7 +4,6 @@ import { cookies } from "next/headers";
 import { JWT_COOKIE_NAME } from "@/constants/appConstants";
 import { LOGIN_URL, REGISTER_URL } from "@/constants/apiUrls";
 import { LoginRequest, RegisterRequest, AuthenticationResponse, ExceptionResponse } from "@/types/authTypes";
-import { handleErrors } from "@/lib/api_utils";
 
 export const login = async (values: LoginRequest) => {
 	const response = await fetch(LOGIN_URL, {
@@ -29,25 +28,25 @@ export const login = async (values: LoginRequest) => {
 };
 
 export const register = async (values: RegisterRequest) => {
-	try {
-		const response = await fetch(REGISTER_URL, {
-			method: "POST",
-			headers: { "Content-Type": "application/json" },
-			body: JSON.stringify(values),
-		});
+	const response = await fetch(REGISTER_URL, {
+		method: "POST",
+		headers: { "Content-Type": "application/json" },
+		body: JSON.stringify(values),
+	});
 
-		if (response.status === 401) {
-			const data: ExceptionResponse = await response.json();
-			return { error: data.message };
-		}
+	if (response.status === 404) {
+		return { error: "Ocurrio un error, intente mas tarde." };
+	}
 
-		if (response.status === 201) {
-			const cookieStore = await cookies();
-			const data: AuthenticationResponse = await response.json();
-			cookieStore.set(JWT_COOKIE_NAME, data.jwt, { httpOnly: true });
-		}
-	} catch (error) {
-		return handleErrors(error as Error);
+	if (response.status === 401) {
+		const data: ExceptionResponse = await response.json();
+		return { error: data.message };
+	}
+
+	if (response.status === 201) {
+		const cookieStore = await cookies();
+		const data: AuthenticationResponse = await response.json();
+		cookieStore.set(JWT_COOKIE_NAME, data.jwt, { httpOnly: true });
 	}
 };
 
